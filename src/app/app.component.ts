c314a0f1601d63509aa984eee5a567b74826f095
esaniello <shaniel.samadhan.natin@gmail.com>
2019-03-18 19:10:36 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    selector: "ns-app",
+    templateUrl: "app.component.html",
+    styleUrls: ["./app.component.css"]
+})
+export class AppComponent { 
+    value = 0;
+
+    onButtonTap(){
+        this.value++;
+    }
+}
